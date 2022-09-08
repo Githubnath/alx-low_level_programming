e5f304d1fff4b884b74c25ef45dea37e16cf1d78
“Emenike <emenikenathaniel55@gmail.com>
2022-09-08 03:22:34 -0700

    c programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,7 +11,7 @@ int main(void)
 {
 	int w;
 
-	w = write(STDOUT_FILENO, "\" -\n", 1);
+	w = write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 10);
 	if (w < 0)
 	{
 		perror("Writing error: ");
