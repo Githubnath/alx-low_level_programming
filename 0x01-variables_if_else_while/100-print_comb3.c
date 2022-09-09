020033415d0804257646953e09a958cedb24f44c
“Emenike <emenikenathaniel55@gmail.com>
2022-09-09 04:24:08 -0700

    c programming

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,27 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
+
 /**
- * main - Entry point
- * Return: Always 0 (Success/completed)
+ * main - Entry point for writing entire base10 single digits
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int number_left;
-	int number_right;
+		int integer;
 
-	for (number_left = 48; number_right <= 78; number_left++)
-	{
-		for (number_right = number_left + 1 ; number_right <= 78; number_right++)
-		{
-		putchar(number_left);
-		putchar (number_right);
-		if ((number_left == 56) && (number_right == 78))
-		{
-		break;
-		}
-		putchar(',');
-		putchar (' ');
-		}
-	}
-	putchar('\n');
-	return (0);
+		for (integer = 0 ; integer <= 9 ; integer++)
+					{
+			putchar(integer + '0');
+			if (integer != 9)
+			{
+				putchar(',');
+				putchar(' ');
+				}
+					}
+		putchar('\n');
+		return (0);
 }
